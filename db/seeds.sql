19612029ec61047a88d7d7ea755fb18a4726f12c
Benjamin Turnbull <benorule@outlook.com>
2020-08-12 12:20:47 +0930

    create connection.js file with code to connect node to mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Big Mac');
 INSERT INTO burgers (burger_name) VALUES ('McChicken');
-INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
+
