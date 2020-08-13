a14b4fa4bd80000186f0cab98992b4e99e4f7f93
Benjamin Turnbull <benorule@outlook.com>
2020-08-14 02:21:56 +0930

    create orm function call for method that updates data inside database in burger object in burger.js file

--- models/burger.js
+++ models/burger.js
@@ -13,6 +13,11 @@ var burger = {
             cb(res);
         });
     },
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      }
 };
 
 module.exports = burger; // export object calls
