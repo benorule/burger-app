72ca6aa567a3c0db7d0fc58ec9c5319c389d885b
Benjamin Turnbull <benorule@outlook.com>
2020-08-14 02:39:07 +0930

    create route for insert data

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const burger = require('../models/burger.js'); // import burger.js
 
 // routes and login within them
+// route for select method
 router.get("/", function(req, res) {
     burger.select(function(data) {
         var hbsObject = {
@@ -12,3 +13,13 @@ router.get("/", function(req, res) {
         res.render("index", hbsObject);
     });
 });
+
+// route for insert method
+router.post("/api/burgers", function(req, res) {
+    burgers.insert(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
+
+// export routes
+module.exports = router;
