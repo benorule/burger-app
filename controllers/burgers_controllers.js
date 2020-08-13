a9a5c8b2e77fd28efc06bafe750d94b2434a4b5a
Benjamin Turnbull <benorule@outlook.com>
2020-08-14 02:34:25 +0930

    create route for select data

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,2 +1,14 @@
 const express = require('express');
+const router = express.Router();
 const burger = require('../models/burger.js'); // import burger.js
+
+// routes and login within them
+router.get("/", function(req, res) {
+    burger.select(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
