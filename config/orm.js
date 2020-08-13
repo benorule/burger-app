4b3aec23e39aad9ad673528cbf5fc4e273307d50
Benjamin Turnbull <benorule@outlook.com>
2020-08-13 23:34:56 +0930

    create method to retrieve data from databse in orm object in orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,21 @@
-const con = require('../config/connection.js'); // import connection.js
+const connection = require('../config/connection.js'); // import connection.js
 
 // object containing all sql statement functions/methods
 var orm = {
-    all: function() {
+    // method to retrieve data from database
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM" + table + ";"; // MySQL query to select everything from table
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function() {
 
     },
-    create: function() {
-
-    },
-    update: function() {
+    updateOne: function() {
     }
 };
 
