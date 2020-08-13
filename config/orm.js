9fd8e5e681a122251df82d272fd141f44d2acf5c
Benjamin Turnbull <benorule@outlook.com>
2020-08-13 17:00:32 +0930

    create empty functions for SQL statements in orm.js file

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,14 @@ const con = require('../config/connection.js'); // import connection.js
 
 // object containing all sql statement functions/methods
 var orm = {
+    all: function() {
 
+    },
+    create: function() {
+
+    },
+    update: function() {
+    }
 };
 
 module.exports = orm; // export object containing methods
