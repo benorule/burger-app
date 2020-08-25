2ae58c9276713429ccdf08fd00683a26fbebefa0
unknown <benorule@outlook.com>
2020-08-25 18:23:53 +0930

    set up deployment for jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 var mysql = require('mysql'); // import sql package
 
 // connect to sql database
-var con = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "myRootPassword",
-  database: "burgers_db",
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "myRootPassword",
+//   database: "burgers_db",
+// });
+
+var con = mysql.createConnection(process.env.JAWSDB_URL);
 
 // test connection
 con.connect(function(err) {
